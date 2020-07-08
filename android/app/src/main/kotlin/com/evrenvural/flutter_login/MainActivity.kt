981d51ddc05633b48e14d0bb046645442bfd1f37
evrenvural <evrenvural4@gmail.com>
2020-07-08 22:09:53 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/evrenvural/flutter_login/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.evrenvural.flutter_login
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_login/login_view.dart';
+
+void main() => runApp(Main());
+
+class Main extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: LoginView(),
+    );
+  }
+}
